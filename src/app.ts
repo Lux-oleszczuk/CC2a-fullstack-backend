1c8ebad1ed126687c5a976b339d88f505bd6e042
Luxana <luxana.oleszczuk@student.norwichuni.ac.uk>
2025-11-04 10:14:16 +0000

    .

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false })); // parse url encoded reques
 
 // hello world response
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello World!' })
+  res.send({ message: 'Why do I have to suffer?!' })
 })
 
 // start server
